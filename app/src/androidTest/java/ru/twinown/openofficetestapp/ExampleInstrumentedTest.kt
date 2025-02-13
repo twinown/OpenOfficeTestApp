4323fb35e1af5a1f6b18133e7d7ca92e6edff5d8
twinown <niyaz9797@gmail.com>
2025-02-13 15:57:57 +0300

    Initial commit

---
+++ app/src/androidTest/java/ru/twinown/openofficetestapp/ExampleInstrumentedTest.kt
@@ -0,0 +1,14 @@
+package ru.twinown.openofficetestapp
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+}
